666b7bd388a304004a3faf353377bb65b79fc5c8
johnnyqian <moneyriver2006@gmail.com>
2014-11-08 11:02:19 +0800

    Merge binary search tree

---
+++ BinaryTree/MergeBinarySearchTree/BSTree.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef int DataType;
+typedef struct BSTreeNode{
+    DataType data;
+    struct BSTreeNode *lchild, *rchild;
+} BSTree;
+
+void insert(BSTree* curr, BSTree* tmp);
+
+BSTree* createTree()
+{
+    BSTree *root, *tmp;
+    root = NULL;
+
+    int x;
+    while (cin >> x && x!=-1)
+    {
+        tmp = (BSTree *)malloc(sizeof(BSTree));
+        tmp->data = x;
+        tmp->lchild = tmp->rchild = NULL;
+
+        if (!root)
+            root = tmp;
+        else
+            insert(root, tmp);
+    }
+    return root;
+}
+
+void insert(BSTree* curr, BSTree* tmp)
+{
+    if (tmp->data < curr->data)
+    {
+        if (!curr->lchild)
+            curr->lchild = tmp;
+        else
+            insert(curr->lchild, tmp);
+    }
+    else if (tmp->data > curr->data)
+    {
+        if (!curr->rchild)
+            curr->rchild = tmp;
+        else
+            insert(curr->rchild, tmp);
+    }
+    else
+    {
+        // duplicate value is not allowed in binary search tree
+        cerr << "duplicate value is not allowed!" << endl;
+        exit(1);
+    }
+}
+
+// traverse with inorder
+void inorder(BSTree *p)
+{
+    if (p != NULL)
+    {
+        inorder(p->lchild);
+        cout << p->data << "\t";
+        inorder(p->rchild);
+    }
+}
---
+++ BinaryTree/MergeBinarySearchTree/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include "BSTree.cpp"
+#include <iostream>
+
+using namespace std;
+
+void merge(BSTree *s, BSTree *p);
+
+int main()
+{
+    freopen("Input.txt", "r", stdin);
+
+    BSTree *A, *B;
+    A = createTree();
+    B = createTree();
+
+    cout << "BST A:\t";
+    inorder(A);
+    cout << endl;
+
+    cout << "BST B:\t";
+    inorder(B);
+    cout << endl;
+
+    merge(A, B);
+
+    cout << "Merged BST:\n";
+    inorder(A);
+    cout << endl;
+}
+
+void merge(BSTree *s, BSTree *p)
+{
+    if (p)
+    {
+        merge(s, p->lchild);
+
+        BSTree* tmp = (BSTree *)malloc(sizeof(BSTree));
+        tmp->data = p->data;
+        tmp->lchild = tmp->rchild = NULL;
+        insert(s, tmp);
+
+        merge(s, p->rchild);
+    }
+}
