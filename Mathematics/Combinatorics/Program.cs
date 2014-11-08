aa4a13cab35f85f0c74dc8a6598cd3d4cc4b42c8
johnnyqian <moneyriver2006@gmail.com>
2014-11-08 21:31:46 +0800

    Permutation and Combination

---
+++ Mathematics/Combinatorics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Combinatorics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine(Combinatorics.CombinationCount(arr.Length, 3));
+            Console.WriteLine(Combinatorics.PermutationCount(arr.Length, 3));
+            Console.WriteLine();
+
+            var allCombination = Combinatorics<int>.GetCombination(arr, 3);
+            foreach (var item in allCombination)
+            {
+                foreach (var i in item)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\t");
+            }
+
+            Console.WriteLine("\n");
+
+            var allPermutation = Combinatorics<int>.GetPermutation(arr, 3);
+            foreach (var item in allPermutation)
+            {
+                foreach (var i in item)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\t");
+            }
+        }
+    }
+}
---
+++ Mathematics/Combinatorics/Readme.txt
@@ -0,0 +1,10 @@
+﻿组合数学中的排序与组合
+
+
+A-Arrangement 排列数（在旧教材为P-Permutation），P(N,R)
+C-Combination 组合数，C(N,R)
+
+References:
+http://en.wikipedia.org/wiki/Permutation
+http://www.cnblogs.com/shuaiwhu/archive/2012/04/27/2473788.html
+http://www.cnblogs.com/snowdust/archive/2010/01/20/1652161.html
