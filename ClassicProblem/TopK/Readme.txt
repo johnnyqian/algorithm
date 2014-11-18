71605ed293064538b8335af04d384bf32fa5d4bb
johnnyqian <moneyriver2006@gmail.com>
2014-11-18 22:16:36 +0800

    TopK problem

---
+++ ClassicProblem/TopK/Readme.txt
@@ -0,0 +1,5 @@
+﻿Top K问题: 求数组中的前K个最大的数
+
+References:
+http://blog.csdn.net/feliciafay/article/details/12252727
+http://www.cnblogs.com/aboutblank/archive/2012/03/09/2387785.html
---
+++ ClassicProblem/TopK/Source.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <time.h>
+#include "HeapSort.h"
+
+using namespace std;
+
+int main()
+{
+    int n = 1000;
+    int* arr = (int*)malloc(sizeof(int)*n);
+
+    // seed the random-number generator with current time and generate a large set of data
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand();
+    }
+
+    cout << "Original " << n << " numbers:" << endl;
+    print(arr, n);
+
+    int k = 10;
+    int* result = (int*)malloc(sizeof(int)*k);
+    for (int i = 0; i < k; i++)
+    {
+        result[i] = arr[i];
+    }
+
+    init_heap(result, k); // initialize a min-heap
+
+    for (int i = k; i < n; i++)
+    {
+        if (arr[i] > result[0])
+        {
+            result[0] = arr[i];
+            min_heapify(result, k, 0);
+        }
+    }
+
+    // sort
+    for (int i = k - 1; i > 0; i--)
+    {
+        swap(result[0], result[i]);
+        min_heapify(result, i, 0); // heapify the heap from root node
+    }
+
+    cout << "Top " << k << " numbers:" << endl;
+    print(result, k);
+}
